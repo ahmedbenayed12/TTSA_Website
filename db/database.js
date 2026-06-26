5cc10e8a1afc154dd35e9959a043817f928abe55
Ahmed Ben Ayed <ahmedbenayed12@gmail.com>
2026-06-26 16:42:17 +0100

    Make database path configurable

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const Database = require('better-sqlite3');
 const path = require('path');
 const fs = require('fs');
 
-const DB_PATH = path.join(__dirname, '..', 'ttsa.db');
+const DB_PATH = process.env.DATABASE_PATH || path.join(__dirname, '..', 'ttsa.db');
 
 const db = new Database(DB_PATH);
 db.pragma('journal_mode = WAL');
